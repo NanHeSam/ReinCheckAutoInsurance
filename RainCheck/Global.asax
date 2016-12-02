cf7b88bcab6e15c1161d60f3395883cafec0a447
NanHeSam <samhenan@gmail.com>
2016-12-02 12:09:25 -0500

    project init

---
+++ RainCheck/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RainCheck.MvcApplication" Language="C#" %>
---
+++ RainCheck/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RainCheck.Startup))]
+namespace RainCheck
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
